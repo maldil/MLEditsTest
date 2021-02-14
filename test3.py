dab8a919a7861c40a8a7e2fc259d82f563273a7e
Malinda <malinda.dilhara@gmail.com>
2021-02-13 18:21:55 -0800

    changed numpy to forloop

--- test3.py
+++ test3.py
@@ -3,7 +3,9 @@ import numpy
 class Test ():
     def add_arrays(self):
         M = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        add = numpy.sum(M)
+        add = 0
+        for x in M:
+            add+=x
         sum = add*2
         return sum
 
