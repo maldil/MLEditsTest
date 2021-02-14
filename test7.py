0e756a0a518c5f44d80773eace0fc95223aa9ac0
Malinda <malinda.dilhara@gmail.com>
2021-02-13 23:04:04 -0800

    numpy to for

--- test7.py
+++ test7.py
@@ -3,9 +3,11 @@ import numpy
 
 class Test6 ():
     def add_arrays(self):
-        F = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        add = numpy.sum(F)
-        cum = add*2
+        H = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        fff = 0
+        for a in H:
+            fff += a
+        cum = fff * 2
         return cum
 
 
