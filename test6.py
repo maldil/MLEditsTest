1f94c4a3f1faac830b5c7a043d95b421ac04cedb
Malinda <malinda.dilhara@gmail.com>
2021-02-13 18:43:08 -0800

    numpy to for

--- test6.py
+++ test6.py
@@ -4,9 +4,11 @@ import numpy
 class Test6 ():
     def add_arrays(self):
         Q = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        ccc = numpy.sum(Q)
-        result=ccc*2
-        return result
+        bbb = 0
+        for a in Q:
+            bbb += a
+        cum = bbb * 2
+        return cum
 
 
 if __name__ == "__main__":
