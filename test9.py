394e2ccf0c3f3953e1d16553fe1a3e87da19e508
Malinda <malinda.dilhara@gmail.com>
2021-02-15 12:07:33 -0800

    first two commits

---
+++ test9.py
@@ -0,0 +1,29 @@
+
+import numpy
+
+class Test6 ():
+    def add_arrays(self):
+        B = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        C = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        D = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        A = B+C+D
+
+        L = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        sss = numpy.sum(L)
+        cum = sss*2
+
+        P = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        Q = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        R = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        S = P + Q + R
+
+        K = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        sss = numpy.sum(K)
+        loo = sss*100
+
+        return S*cum
+
+
+if __name__ == "__main__":
+    xxx = Test6().add_arrays()
+    print(xxx)
