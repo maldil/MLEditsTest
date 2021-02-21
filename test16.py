a5e18f3e84c5478a394dad975cf5f5bf7c6f2452
Malinda <malinda.dilhara@gmail.com>
2021-02-21 15:43:30 -0800

    file add

--- test16.py
+++ test16.py
@@ -7,7 +7,6 @@ class Test6 ():
         H = [numpy.ndarray([1, 2, 3, 4, 5, 6, 7, 8, 9]),numpy.ndarray([1, 2, 3, 4, 5, 6, 7, 8, 9]),numpy.ndarray([1, 2, 3, 4, 5, 6, 7, 8, 9])]
 
 
-
 if __name__ == "__main__":
     xxx = Test6().add_arrays()
     print(xxx)
