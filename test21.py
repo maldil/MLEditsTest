df3b27939b34406df261f8d1fee0ba3675c3d59f
Malinda <malinda.dilhara@gmail.com>
2021-02-22 22:29:45 -0800

    add

---
+++ test21.py
@@ -0,0 +1,13 @@
+
+import numpy
+import tensorflow
+
+class Test6 ():
+    def add_arrays(self):
+        F = [[1, 2, 3, 4, 5, 6, 7, 8, 9],[1, 2, 3, 4, 5, 6, 7, 8, 9],[1, 2, 3, 4, 5, 6, 7, 8, 9]]
+        D = F*4
+        return D
+
+if __name__ == "__main__":
+    xxx = Test6().add_arrays()
+    print(xxx)
