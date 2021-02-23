88055a0e70198e62ff4c252e45979ddae34f5013
Malinda <malinda.dilhara@gmail.com>
2021-02-22 22:24:01 -0800

    add

---
+++ test20.py
@@ -0,0 +1,13 @@
+
+import numpy
+import tensorflow
+
+class Test6 ():
+    def add_arrays(self):
+        Q = ([[1, 2, 3, 4, 5, 6, 7, 8, 9],[1, 2, 3, 4, 5, 6, 7, 8, 9],[1, 2, 3, 4, 5, 6, 7, 8, 9]]
+        F = Q*4
+        return F
+
+if __name__ == "__main__":
+    xxx = Test6().add_arrays()
+    print(xxx)
