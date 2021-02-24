08135aadc4d832676f9bcb814eaf64dbd98525a1
Malinda <malinda.dilhara@gmail.com>
2021-02-23 17:30:17 -0800

    message

--- test22.py
+++ test22.py
@@ -6,8 +6,7 @@ class Test6 ():
     def add_arrays(self):
         data={3,4}
         collection = [1, 2, 2, 3]
-        for elem in collection:
-            data.add(elem)
+        data.update(collection)
         return data
 if __name__ == "__main__":
     xxx = Test6().add_arrays()
