648b49c9882fbb236a24ae75e9a755f5adb533cf
Malinda <malinda.dilhara@gmail.com>
2021-03-15 01:20:54 -0700

    files

--- package/test34.py
+++ package/test34.py
@@ -2,7 +2,7 @@
 import numpy
 
 class Test1 ():
-    def add_arrays_new(self):
+    def add_arrays(self):
         Z = numpy.ndarray([1, 2, 3, 4, 5, 6, 7, 8, 9])
         bbb = 0
         for y in Z:
