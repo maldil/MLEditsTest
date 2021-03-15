64df6e16440c87417795e024e916b7064e1a4cbf
Malinda <malinda.dilhara@gmail.com>
2021-03-15 01:20:41 -0700

    files

---
+++ package/test34.py
@@ -0,0 +1,16 @@
+
+import numpy
+
+class Test1 ():
+    def add_arrays_new(self):
+        Z = numpy.ndarray([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        bbb = 0
+        for y in Z:
+            bbb += y
+        sum = bbb * 2
+        return sum
+
+
+if __name__ == "__main__":
+    xxx = Test1().add_arrays_new()
+    print(xxx)
