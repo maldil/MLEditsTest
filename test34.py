7df11cb1d8240ad510e50eaba7f194f6d97f6d14
Malinda <malinda.dilhara@gmail.com>
2021-03-18 23:53:51 -0700

    extract method

--- test34.py
+++ test34.py
@@ -4,11 +4,14 @@ import numpy
 class Test1 ():
     def add_arrays_new(self):
         Z = numpy.ndarray([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        bbb = self.sum_array(Z)
+        sum = bbb * 2
+
+    def sum_array(self, Z):
         bbb = 0
         for y in Z:
             bbb += y
-        sum = bbb * 2
-
+        return bbb
 
 
 if __name__ == "__main__":
