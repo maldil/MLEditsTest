681ee1c8e761b20c35117858d0619a9ce768669a
Malinda <malinda.dilhara@gmail.com>
2023-03-20 01:17:05 -0700

    Update test11.py

--- test11.py
+++ test11.py
@@ -30,6 +30,33 @@ class Test6 ():
 
         return S*cum
 
+    
+def add_arrays123(self):
+        B = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        C = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        A = B+C+D
+
+        L = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        ddd = 0
+        for a in L:
+            ddd += a
+        cum = ddd * 2
+        B = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        C = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        C = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        D = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        V = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        S = P + Q + R
+
+        K = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        ooo = 0
+        for a in K:
+            ooo += a
+
+        loo = ooo*100
+
+        return S*cum    
 
 if __name__ == "__main__":
     xxx = Test6().add_arrays()
